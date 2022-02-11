34ec19c5b2bb08af244ce5314b9b899baef59735
Uriel Aispuro <urielalexis64@gmail.com>
2022-02-11 15:04:57 -0700

    finish signup screen and logical

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,69 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'dart:io';
 
-class AuthController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/models/user.dart' as model;
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+
+  Rx<File?> _pickedImage = Rx(null);
+
+  File? get pickedImage => _pickedImage.value;
+
+  void pickImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage == null) return Future.error('No image selected');
+    _pickedImage = Rx<File?>(File(pickedImage.path));
+    Get.snackbar('Profile picture', 'Your profile picture has been updated');
+  }
+
+  // Register user
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // Save out user to auth and firebase firestore
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+          uid: cred.user!.uid,
+          name: username,
+          email: email,
+          profilePhoto: downloadUrl,
+        );
+        await firestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
+        Get.snackbar('Account created', 'Account created succesfully');
+      } else {
+        Get.snackbar(
+            'Error creating an account', 'Please enter all the fields');
+      }
+    } catch (e) {
+      Get.snackbar('Error creating an account', e.toString());
+    }
+  }
+
+  // Upload to Firebase Storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics/${firebaseAuth.currentUser!.uid}');
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String url = await snap.ref.getDownloadURL();
+    return url;
+  }
+}
--- lib/views/screens/auth/signup_screen.dart
+++ lib/views/screens/auth/signup_screen.dart
@@ -13,91 +13,102 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('UriTok',
-              style: TextStyle(
-                  fontSize: 35,
-                  color: buttonColor,
-                  fontWeight: FontWeight.w900)),
-          const Text('Register',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
-          const SizedBox(height: 15),
-          Stack(
-            children: [
-              const CircleAvatar(
-                radius: 64,
-                backgroundImage: NetworkImage(
-                    'https://cdn-icons-png.flaticon.com/512/149/149071.png'),
-                backgroundColor: Colors.black,
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('UriTok',
+                style: TextStyle(
+                    fontSize: 35,
+                    color: buttonColor,
+                    fontWeight: FontWeight.w900)),
+            const Text('Register',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
+            const SizedBox(height: 15),
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      'https://cdn-icons-png.flaticon.com/512/149/149071.png'),
+                  backgroundColor: Colors.black,
+                ),
+                Positioned(
+                    bottom: -10,
+                    right: 0,
+                    child: IconButton(
+                      icon: const Icon(Icons.camera_alt),
+                      onPressed: authController.pickImage,
+                    ))
+              ],
+            ),
+            const SizedBox(height: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _usernameController,
+                icon: Icons.person,
+                label: 'Username',
               ),
-              Positioned(
-                  bottom: -10,
-                  right: 0,
-                  child: IconButton(
-                    icon: const Icon(Icons.camera_alt),
-                    onPressed: () {},
-                  ))
-            ],
-          ),
-          const SizedBox(height: 15),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextInputField(
-              controller: _emailController,
-              icon: Icons.person,
-              label: 'Username',
             ),
-          ),
-          const SizedBox(height: 25),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextInputField(
-              controller: _usernameController,
-              icon: Icons.email,
-              label: 'Email',
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _emailController,
+                icon: Icons.email,
+                label: 'Email',
+              ),
             ),
-          ),
-          const SizedBox(height: 25),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextInputField(
-              controller: _passwordController,
-              icon: Icons.lock,
-              label: 'Password',
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _passwordController,
+                icon: Icons.lock,
+                isObscure: true,
+                label: 'Password',
+              ),
             ),
-          ),
-          const SizedBox(height: 30),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .9,
-            height: 50,
-            child: Material(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              color: buttonColor,
-              child: InkWell(
-                onTap: () {},
-                child: const Center(
-                  child: Text('Register',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .9,
+              height: 50,
+              child: Material(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                color: buttonColor,
+                child: InkWell(
+                  onTap: () {
+                    final username = _usernameController.text;
+                    final email = _emailController.text;
+                    final password = _passwordController.text;
+
+                    authController.registerUser(
+                        username, email, password, authController.pickedImage);
+                  },
+                  child: const Center(
+                    child: Text('Register',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Already have an account? ',
-                  style: TextStyle(
-                    fontSize: 18,
-                  )),
-              Text('Login', style: TextStyle(fontSize: 18, color: buttonColor)),
-            ],
-          )
-        ],
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Already have an account? ',
+                    style: TextStyle(
+                      fontSize: 18,
+                    )),
+                Text('Login',
+                    style: TextStyle(fontSize: 18, color: buttonColor)),
+              ],
+            )
+          ],
+        ),
       )),
     );
   }
