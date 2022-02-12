8345ed7b09e3c676201ab2fcd246cafd6616ed5b
Uriel Aispuro <urielalexis64@gmail.com>
2022-02-11 22:34:10 -0700

    add bottom navigation bar

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:tiktok_clone/views/screens/add_video_screen.dart';
+
+const pages = [
+  Text('HomeScreen'),
+  Text('Search Screen'),
+  AddVideoScreen(),
+  Text('Messages Screen'),
+  Text('ProfileScreen'),
+];
 
 // Colors
 const backgroundColor = Colors.black;
---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/views/widgets/custom_icon.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => setState(() => pageIndex = value),
+        currentIndex: pageIndex,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey[400],
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 24),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 24),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: CustomIcon(),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 24),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 24),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: pages[pageIndex],
+    );
+  }
+}
