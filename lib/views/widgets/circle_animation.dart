1397f803f0d067f00105357c2a9132c829a5636e
Uriel Aispuro <urielalexis64@gmail.com>
2022-02-16 23:18:58 -0700

    add CircleAnimation widget

---
+++ lib/views/widgets/circle_animation.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CircleAnimation extends StatefulWidget {
+  const CircleAnimation({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<CircleAnimation> createState() => _CircleAnimationState();
+}
+
+class _CircleAnimationState extends State<CircleAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+      child: widget.child,
+    );
+  }
+}
